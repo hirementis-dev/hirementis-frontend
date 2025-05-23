31c6336b3d009375c0daaa0a28393d867e6435d7
Suprabhat <sdk26150@gmail.com>
2025-05-23 23:43:08 +0530

    fix again

--- app/api/generate-feedback/route.ts
+++ app/api/generate-feedback/route.ts
@@ -1,15 +1,17 @@
 // import OpenAI from "openai";
 
-// export async function POST(request: Request) {
-//   const { transcript, job_desc } = await request.json();
+export async function POST(request: Request) {
+  const { transcript, job_desc } = await request.json();
 
-//   try {
-//     const formattedTranscript = transcript
-//       .map(
-//         (sentence: { role: string; content: string }) =>
-//           `- ${sentence.role}: ${sentence.content}\n`
-//       )
-//       .join("");
-//     Response.json({ formattedTranscript });
-//   } catch (error) {}
-// }
+  try {
+    const formattedTranscript = transcript
+      .map(
+        (sentence: { role: string; content: string }) =>
+          `- ${sentence.role}: ${sentence.content}\n`
+      )
+      .join("");
+    return Response.json({ formattedTranscript });
+  } catch (error) {
+    console.error("Error formatting transcript:", error);
+  }
+}
